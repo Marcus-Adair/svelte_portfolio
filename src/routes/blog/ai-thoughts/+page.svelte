a2dfc25976aa7db2ec4b05b35ed00cfbe0cacb60
Marcus Adair <46729081+DarkoAdair@users.noreply.github.com>
2025-11-06 20:02:55 -0700

    commit to switch branches

---
+++ src/routes/blog/ai-thoughts/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Paragraph from "../_components/paragraph.svelte";
+	import PostHeader from "../_components/postHeader.svelte";
+
+    const DATE = new Date("2025-11-04T00:40:37.654Z")
+</script>
+
+
+<div class="flex flex-col gap-4">
+    <PostHeader date={DATE} postNum={2} title="AI Thoughts"/>
+
+    <Paragraph>
+        TODO:
+    </Paragraph>
+</div>
